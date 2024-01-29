e28a3ad5a1ab2bfa9b5a293b08732e99e19f038b
Sricharan2211 <tudugani.charan@mphasis.com>
2024-01-29 12:42:11 +0530

    Test

---
+++ InsuranceManagementSystem/UI/App_Start/UnityConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity.Core.Metadata.Edm;
+using System.Web.Mvc;
+using DAL.Data;
+using DAL.Repository;
+using Unity;
+using Unity.AspNet.Mvc;
+
+namespace UI
+{
+    public static class UnityConfig
+    {
+        public static IUnityContainer Container { get; internal set; }
+
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IAdminRepository, AdminRepository>();
+
+            container.RegisterType<ICustomerRepository, CustomerRepository>();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            Container=container;
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
